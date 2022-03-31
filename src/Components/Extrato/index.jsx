ae28946755b0cc21b71941904bc4f612cce13551
Daniel Carneiro <daniel.alexandre97@hotmail.com>
2022-03-31 20:17:55 -0300

    feat: implementing styled components, global styles, dark and light themes

---
+++ src/Components/Extrato/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { extratoLista } from "../../info";
+import Items from "../Items";
+import { Botao, Box } from "../UI";
+
+const Extrato = () => {
+  return (
+    <Box>
+      {extratoLista.updates.map(({ id, type, from, value, date }) => {
+        return (
+          <Items key={id} type={type} from={from} value={value} date={date} />
+        );
+      })}
+      <Botao>Ver Mais</Botao>
+    </Box>
+  );
+};
+
+export default Extrato;
---
+++ src/Components/UI/variaveis.js
@@ -0,0 +1,9 @@
+export const corPrimaria = "#41D3BE";
+
+export const fundoClaro = "#F1F1F1";
+export const textoFundoClaro = "grey";
+export const conteudoClaro = "white";
+
+export const fundoEscuro = "#363537";
+export const textoFundoEscuro = "#FAFAFA";
+export const conteudoEscuro = "#5C5B5E";
